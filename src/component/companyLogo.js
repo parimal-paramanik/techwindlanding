d28fcdb4cf4c85096dd735c35ad252706d9b2a46
unknown <smartdesk2015@gmail.com>
2024-02-29 17:25:14 +0530

    chnages

--- src/component/companyLogo.js
+++ src/component/companyLogo.js
@@ -1,19 +1,19 @@
 import React from "react";
-import amazon from '../assets/images/client/amazon.svg';
-import google from '../assets/images/client/google.svg';
-import lenovo from '../assets/images/client/lenovo.svg';
-import paypal from '../assets/images/client/paypal.svg';
-import shopify from '../assets/images/client/shopify.svg';
-import spotify from '../assets/images/client/spotify.svg';
+import amazon from '../assets/images/client/delivery.png';
+import google from '../assets/images/client/delhiveylogo.png';
+import lenovo from '../assets/images/client/iitdelhi.png';
+import paypal from '../assets/images/client/kaggle-logo.png';
+import shopify from '../assets/images/client/credmark-logo.png';
+import spotify from '../assets/images/client/cii-logo.png';
 export default function CompanyLogo(){
     return(
         <div className="grid md:grid-cols-6 grid-cols-2 justify-center gap-[30px]">
             <div className="mx-auto py-4">
-                <img src={amazon} className="h-6" alt="" />
+                <img src={google} className="h-6" alt="" />
             </div>
 
             <div className="mx-auto py-4">
-                <img src={google} className="h-6" alt="" />
+                <img src={amazon} className="h-6" alt="" />
             </div>
 
             <div className="mx-auto py-4">

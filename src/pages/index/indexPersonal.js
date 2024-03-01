72311025169253fdf9cad400542ef57c9eabc0d7
unknown <smartdesk2015@gmail.com>
2024-03-01 15:43:56 +0530

    education experience added

--- src/pages/index/indexPersonal.js
+++ src/pages/index/indexPersonal.js
@@ -14,22 +14,8 @@ export default function IndexPersonal() {
 
                     <div className="grid grid-cols-1 mt-8">
                         <div className="relative after:content-[''] after:absolute after:top-0 md:after:end-0 md:after:start-0 after:w-px after:h-full md:after:m-auto after:border-s-2 after:border-dashed after:border-gray-200 dark:after:border-gray-700 ms-3 md:ms-0">
-                            <div className="ms-8 md:ms-0 relative after:content-[''] after:absolute after:top-[9px] after:rounded-full after:z-10 after:w-2.5 after:h-2.5 after:bg-indigo-600 md:after:mx-auto md:after:end-0 md:after:start-0 after:-start-9 before:content-[''] before:absolute md:before:mx-auto md:before:end-0 md:before:start-0 before:-start-11 before:rounded-full before:z-10 before:border-2 before:border-dashed before:border-gray-200 dark:before:border-gray-700 before:top-0 before:w-7 before:h-7 before:bg-white dark:before:bg-slate-900">
-                                <div className="grid md:grid-cols-2">
-                                    <div className="md:text-end md:me-8 relative">
-                                        {/* <img src={shree_logo} className="rounded-full h-9 w-9 md:ms-auto" alt="" /> */}
-                                        <h5 className="my-2 font-semibold text-lg">Chief Technology Officer</h5>
-                                        <h6 className="text-slate text-sm mb-0">Oct 2021 - Present</h6>
-                                    </div>
-
-                                    <div className="ltr:float-left rtl:float-right text-start ms-8 mt-6 md:mt-0">
-                                        <h5 className="title mb-1 font-semibold">ZeeQ Technologies</h5>
-                                       
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div className="mt-12 ms-8 md:ms-0 relative after:content-[''] after:absolute after:top-[9px] after:rounded-full after:z-10 after:w-2.5 after:h-2.5 after:bg-indigo-600 md:after:mx-auto md:after:end-0 md:after:start-0 after:-start-9 before:content-[''] before:absolute md:before:mx-auto md:before:end-0 md:before:start-0 before:-start-11 before:rounded-full before:z-10 before:border-2 before:border-dashed before:border-gray-200 dark:before:border-gray-700 before:top-0 before:w-7 before:h-7 before:bg-white dark:before:bg-slate-900">
+                           
+                            <div className="mt-1 ms-8 md:ms-0 relative after:content-[''] after:absolute after:top-[9px] after:rounded-full after:z-10 after:w-2.5 after:h-2.5 after:bg-indigo-600 md:after:mx-auto md:after:end-0 md:after:start-0 after:-start-9 before:content-[''] before:absolute md:before:mx-auto md:before:end-0 md:before:start-0 before:-start-11 before:rounded-full before:z-10 before:border-2 before:border-dashed before:border-gray-200 dark:before:border-gray-700 before:top-0 before:w-7 before:h-7 before:bg-white dark:before:bg-slate-900">
                                 <div className="grid md:grid-cols-2">
                                     <div className="text-start ms-8 relative md:order-2">
                                         {/* <img src={google_logo} className="rounded-full h-9 w-9 md:me-auto" alt="" /> */}
